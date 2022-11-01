4818d7c7f707b62c92fee544784a632288a04a5b
XuTang9 <xutang@ucsd.edu>
2022-11-01 01:12:07 -0700

    1

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,12 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const audio = document.querySelector('.hidden');
+  const horn_select = document.getElementById('horn-select');
+
+  horn_select.addEventListener('change', (event) =>{
+      const pic = document.querySelector('img');
+      pic.src = "assets/images/" + `${event.target.value}` + ".svg";
+      audio.src = "assets/audio/" + `${event.target.value}` + ".mp3";
+  });
 }
